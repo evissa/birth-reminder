010cafd6728910bc8151b889c57adf6aa3044f38
Evisa Cankaj <cankaj17@live.com>
2023-10-22 16:25:06 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Evisa Cankaj',
+    age: 27,
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/99/99f7f5c3f1f6fb41e15bc3afccddee0437f51549.jpg',
+  },
+  {
+    id: 2,
+    name: 'Lorenc C',
+    age: 34,
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/06/06da8a4f0fa669110c3dbd0efc8d269b1f57c511.jpg',
+  },
+  {
+    id: 3,
+    name: 'Ilda C',
+    age: 24,
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/78/785d25dca97559f70133087babfcaa41560182ac.jpg',
+  },
+  {
+    id: 4,
+    name: 'Kleart C',
+    age: 35,
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/f5/f58bb3caf5a66deeb85ab9c6e0cf2024057b8331.jpg',
+  },
+  {
+    id: 5,
+    name: 'Melisa M',
+    age: 29,
+    image:
+      'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/20/2039a4433e791f47ae7e60a0f2acdae73b26bd6a.jpg',
+  },
+];
